b12dbb12eab4852a73b3a3ecb8d81fe3c527a8ae
darya <darya.kolycheva98@yandex.ru>
2024-07-13 00:56:20 +0300

    ДЗ готово

---
+++ src/main/java/ru/skypro/calculator/exception/CalculatorException.java
@@ -0,0 +1,8 @@
+package ru.skypro.calculator.exception;
+
+public class CalculatorException extends IllegalArgumentException {
+    public CalculatorException(String s) {
+        super(s);
+    }
+}
+
